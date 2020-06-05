83dbd4b9f355724b60bce88f0bacc9bee812b770
Caroline <cj.jin@hotmail.com>
2020-06-05 19:45:11 -0400

    First Scrape Code

---
+++ scrape_web/scrape1.js
@@ -0,0 +1,12 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+
+axios.get('https://www.foxnews.com/').then((response) =>{
+	const $ = cheerio.load(response.data);
+	let altElems = [];
+	$('img').each(function (i, e){
+		altElems[i] = $(this).attr('alt');
+	})
+	fs.writeFile('output.txt', altElems);
+})
