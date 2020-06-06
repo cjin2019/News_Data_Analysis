df73fee890a353189d6b172fbe4e529c20ceab68
2019cjin <csj3sd@virginia.edu>
2020-06-06 16:06:10 -0400

    scrapes for multiple websites

--- python_scrape/constants.py
+++ python_scrape/constants.py
@@ -2,5 +2,6 @@ NEWS_URLS = {
     'fox': 'https://www.foxnews.com/', 
     'nbc': 'https://www.nbcnews.com/',
     'cnn': 'https://www.cnn.com/',
-    'abc': 'https://abcnews.go.com/'
+    'abc': 'https://abcnews.go.com/',
+    'reuters': 'https://www.reuters.com/'
 }
