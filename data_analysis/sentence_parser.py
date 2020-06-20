fe186869e92b49e3a576a14c1fab771b2e92cba8
Caroline <cj.jin@hotmail.com>
2020-06-20 14:43:44 -0400

    Updated sentence_parser to retrieve entities

--- data_analysis/sentence_parser.py
+++ data_analysis/sentence_parser.py
@@ -1,5 +1,6 @@
 import nltk
 import spacy
+import re
 
 class SentenceParser():
 	"""docstring for SentenceParser"""
@@ -8,23 +9,26 @@ class SentenceParser():
 		"""
 		Initialized RegexpParser
 		"""
-		self.pos_tag_model = spacy.load("en_core_web_sm")
+		self.spacy_model = spacy.load("en_core_web_sm")
+		self.word_set = set(nltk.corpus.words.words())
 
 	def part_of_speech_tag(self, sentence):
 		"""
 		Returns a list of tuples (part of speech, word)
 		"""
-		doc = self.pos_tag_model(sentence)
-		return [(token.text, token.tag_) for token in doc]
+		tokenized = nltk.word_tokenize(sentence)
+		return nltk.pos_tag(tokenized)
 
 	def chunk_noun_phrases(self, sentence):
 		"""
 		Returns a list of noun phrases
 		"""
-		grammar = "NP: {<PRP.?>?<JJ.?>*<NN.*>*}"
+		grammar = "NP: {<PRP.?>?<CD>*<JJ.?>*<NN.*>*}"
 		parser = nltk.RegexpParser(grammar)
 		pos_tag = self.part_of_speech_tag(sentence)
-		print(pos_tag)
+		print()
+		print("Sentence:", sentence)
+		print("Part of speech:", pos_tag)
 		chunks = parser.parse(pos_tag)
 
 		noun_phrases = []
@@ -35,4 +39,80 @@ class SentenceParser():
 					noun_phrase_str+=(word + ' ')
 				noun_phrases.append(noun_phrase_str[:-1])
 		return noun_phrases
+
+	def in_restricted_entity(self, label):
+		"""
+		Returns whether the label is in restricted entity
+		"""
+		restricted = {"DATE", "TIME", "ORDINAL", "CARDINAL"}
+		return label in restricted
+
+	def is_common_word(self, word):
+		"""
+		Returns whether the word is in nltk word set
+		"""
+		return word in self.word_set or word.lower() in self.word_set
+
+	def remove_punct(self, text):
+		"""
+		Returns the word without quotation or other punctuation
+		"""
+		if len(text) >=2 and text[-2:]=="'s":
+			text = text[:-2]
+		pattern = "([^\w\s-])"
+		return re.sub(pattern, "", text)
+
+	def is_proper_noun(self, word):
+		"""
+		Returns whether the word is a proper noun
+		"""
+		return not self.is_common_word(word) and not word.islower()
+
+	def helper_not_caught(self, word, words_in_ents):
+		"""
+		Returns whether the words should be added to ents
+		"""
+		return len(word)>0 and word not in words_in_ents and \
+				self.is_proper_noun(word)
+
+	def retrieve_entities_not_caught(self, words_in_ents, sentence):
+		"""
+		Returns a list of entities that should have been added but 
+		was not identified by spacy
+		"""
+		ents = []
+		tokenized = nltk.word_tokenize(sentence)
+
+		for word in tokenized:
+			cleaned_word = self.remove_punct(word)
+			if self.helper_not_caught(cleaned_word, words_in_ents):
+				ents.append((word, "UNLABELED"))
+
+		return ents
+
+	def retrieve_entities(self, sentence):
+		"""
+		Returns a list of people, organizations, countries, etc.
+		Each item is a tuple (text, label)
+		"""
+		ents = []
+		words_in_ents = set()
+
+		for ent in self.spacy_model(sentence).ents:
+			text, label = ent.text, ent.label_
+			if not self.in_restricted_entity(label):
+				print(text)
+				cleaned_text = self.remove_punct(text)
+				ents.append((cleaned_text, label))
+				words_in_ents = words_in_ents | set(cleaned_text.split(" "))
+
+		print(ents)
+		return ents + self.retrieve_entities_not_caught(words_in_ents, sentence)
+
+	def retrieve_keywords(self, sentence):
+		"""
+		Returns the list of entity text
+		"""
+		return [ent[0] for ent in self.retrieve_entities(sentence)]
+
 
--- test_sentence_parser.py
+++ test_sentence_parser.py
@@ -10,6 +10,9 @@ class TestSentenceParser(unittest.TestCase):
 		"""
 		self.parser = SentenceParser()
 
+class TestChunkNounPhrase(TestSentenceParser):
+	"""docstring for TestChunkNounPhrase"""
+
 	def test_case_1(self):
 		sentence = "My name is Caroline"
 		expected = {"My name", "Caroline"}
@@ -23,7 +26,7 @@ class TestSentenceParser(unittest.TestCase):
 		self.assertEqual(res, expected)
 
 	def test_case_3a(self):
-		sentence = "Parents beat 12-year-old boy together"
+		sentence = "Parents beat and stab 12-year-old boy together"
 		expected = {"Parents", "12-year-old boy"}
 		res = set(self.parser.chunk_noun_phrases(sentence))
 		self.assertEqual(res, expected)
@@ -46,6 +49,18 @@ class TestSentenceParser(unittest.TestCase):
 		res = set(self.parser.chunk_noun_phrases(sentence))
 		self.assertEqual(res, expected)
 
+	def test_case_5(self):
+		sentence = "Beijing braces for 2nd wave of COVID-19 infections"
+		expected = {"Beijing", "2nd wave", "COVID-19 infections"}
+		res = set(self.parser.chunk_noun_phrases(sentence))
+		self.assertEqual(res, expected)
+
+	def test_case_6(self):
+		sentence = "Bishop resumes work as Vatican abuse probe wraps"
+		expected = {"Bishop", "work", "Vatican abuse probe"}
+		res = set(self.parser.chunk_noun_phrases(sentence))
+		self.assertEqual(res, expected)
+
 	def test_case_noun_verb_a(self):
 		sentence = "Father, son stab 12-year-old boy together"
 		expected = {"Father", "son", "12-year-old boy"}
@@ -53,9 +68,64 @@ class TestSentenceParser(unittest.TestCase):
 		self.assertEqual(res, expected)
 
 	def test_case_noun_verb(self):
-		sentence = "Father, son beat, stab, and hit 12-year-old boy together"
+		sentence = "Father, son beat and stab 12-year-old boy together"
 		expected = {"Father", "son", "12-year-old boy"}
 		res = set(self.parser.chunk_noun_phrases(sentence))
+		self.assertEqual(res, expected)
+
+class TestEntityRetrieve(TestSentenceParser):
+	"""docstring for TestEntityRetrieve"""
+
+	def test_case_remove_punct(self):
+		text = "'Political Machine'"
+		expected = "Political Machine"
+		res = self.parser.remove_punct(text)
+		self.assertEqual(res, expected)
+
+	def test_case_remove_punct2(self):
+		text = "COVID-19"
+		expected = "COVID-19"
+		res = self.parser.remove_punct(text)
+		self.assertEqual(res, expected)
+
+	def test_case_remove_punct3(self):
+		text = ","
+		expected = ""
+		res = self.parser.remove_punct(text)
+		self.assertEqual(res, expected)
+
+	def test_case_1(self):
+		sentence = "Beijing braces for 2nd wave of COVID-19 infections"
+		expected = {"Beijing", "COVID-19"}
+		res = set(self.parser.retrieve_keywords(sentence))
+		self.assertEqual(res, expected)
+
+	def test_case_2(self):
+		sentence = "Video of man confronted by white couple for \
+					stenciling Black Lives Matter goes viral"
+		expected = {"Black Lives Matter"}
+		res = set(self.parser.retrieve_keywords(sentence))
+		self.assertEqual(res, expected)
+
+	def test_case_3(self):
+		sentence = "Deadly force not 'only option' in Atlanta \
+					shooting: Rep. Val Demings"
+		expected = {"Atlanta", "Val Demings"}
+		res = set(self.parser.retrieve_keywords(sentence))
+		self.assertEqual(res, expected)
+
+	def test_case_4(self):
+		sentence = "Fauci tells ABC's 'Powerhouse Politics' that \
+					attending rallies, protests is 'risky'"
+		expected = {"Fauci", "ABC", "Powerhouse Politics"}
+		res = set(self.parser.retrieve_keywords(sentence))
+		self.assertEqual(res, expected)
+
+	def test_case_5(self):
+		sentence = "John Bolton's book recounts chaos, turmoil \
+					in Trump White House"
+		expected = {"John Bolton", "Trump White House"}
+		res = set(self.parser.retrieve_keywords(sentence))
 		self.assertEqual(res, expected)
 if __name__ == '__main__':
 	unittest.main(verbosity=3, exit =True)
