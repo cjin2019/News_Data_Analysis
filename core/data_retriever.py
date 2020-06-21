b93ed3cacf05cdc29f4a903d748339d2862c9001
2019cjin <csj3sd@virginia.edu>
2020-06-21 17:50:13 -0400

    restructured data_retriever

--- core/data_retriever.py
+++ core/data_retriever.py
@@ -3,31 +3,6 @@ from core.database.data_manager import DataManager
 from data_analysis.sentiment_analyzer import SentimentAnalyzer
 
 class DataRetriever:
-    def add_key_words(self, data_manager, title):
-        keywords = [] # add algorithm to get keywords
-        for keyword in keywords:
-            data_manager.insert_keyword(keyword)
-
-    def add_sentiment_values(self, data_manager, title):
-        sentiment_analyzer = SentimentAnalyzer()
-        vader = sentiment_analyzer.vader_polarity(title)
-        liu_hu = sentiment_analyzer.liu_and_hu_polarity(title)
-
-        data_manager.insert_sentiment_values(title, vader, liu_hu)
-
-    def gather_news_source_data(self, data_manager, news_outlet):
-        cleaner = Cleaner(news_outlet)
-        titles = cleaner.get_titles()
-        for title in titles:
-            print(f'adding {str(title)}')
-            data_manager.insert_headline(str(title), news_outlet)
-
-            #print(f'adding {str(title)}\'s sentiment values')
-            #self.add_sentiment_values(data_manager, title)
-
-            #print(f'adding {str(title)}\'s sentiment values key words')
-            #self.add_key_words(data_manager, title)
-
     def gather_all_data(self):
         data_manager = DataManager()
 
@@ -39,3 +14,28 @@ class DataRetriever:
             )
         
         data_manager.close()
+    
+    def gather_news_source_data(self, data_manager, news_outlet):
+        cleaner = Cleaner(news_outlet)
+        titles = cleaner.get_titles()
+        for title in titles:
+            print(f'adding {str(title)}')
+            data_manager.insert_headline(str(title), news_outlet)
+
+            #print(f'adding {str(title)}\'s sentiment values')
+            #self._add_sentiment_values(data_manager, title)
+
+            #print(f'adding {str(title)}\'s sentiment values key words')
+            #self._add_key_words(data_manager, title)
+
+    def _add_sentiment_values(self, data_manager, title):
+        sentiment_analyzer = SentimentAnalyzer()
+        vader = sentiment_analyzer.vader_polarity(title)
+        liu_hu = sentiment_analyzer.liu_and_hu_polarity(title)
+
+        data_manager.insert_sentiment_values(title, vader, liu_hu)
+    
+    def _add_key_words(self, data_manager, title):
+        keywords = [] # add algorithm to get keywords
+        for keyword in keywords:
+            data_manager.insert_keyword(keyword)
